9da8a723b932f3921b57bc0618d2c79b90febda1
Etinger Nikita <merinkjoe@gmail.com>
2023-04-14 19:44:56 +0300

    start snake

---
+++ NewCWProject/NewCWProject/param.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <iostream>
+#define random(a,b) a+rand()%(b+1-a)
+using namespace std;
+const int SIZE_FIELD = 10;
+const int MARGIN = 5;
+const int MIN_SIZE_SNAKE = 4;
